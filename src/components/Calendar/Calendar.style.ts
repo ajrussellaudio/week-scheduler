7f4142f6be27ae3338b6cec0ca6bd8cb9e471f7d
Alan Russell <ajrussellaudio@gmail.com>
2022-10-17 19:36:08 +0100

    Add calendar styles

---
+++ src/components/Calendar/Calendar.style.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Grid = styled.div`
+  padding: 2rem;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1rem;
+
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(7, 1fr);
+  }
+`;
+
+export const DayName = styled.span`
+  display: block;
+  font-weight: bold;
+  font-size: 1.5rem;
+  margin-bottom: 0.5rem;
+`;
+
+export const EventsList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+export const Event = styled.li`
+  width: 100%;
+  padding: 0.333rem;
+  border-radius: 0.2rem;
+  box-shadow: 0 0 17px 2px rgb(0 0 0 / 10%);
+`;
+
+export const Subject = styled.span`
+  display: block;
+  white-space: nowrap;
+`;
+
+export const Time = styled.span`
+  font-size: 0.666rem;
+  white-space: nowrap;
+`;
--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,8 +1,33 @@
 import { useEvents } from '../../context/Events';
+import { weekdays } from '../../context/Events/types';
+import { DayName, Event, EventsList, Grid, Subject, Time } from './Calendar.style';
 
 export type CalendarProps = Record<string, never>;
 
 export function Calendar() {
   const { state: schedule } = useEvents();
-  return <pre style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(schedule, null, 2)}</pre>;
+  return (
+    <Grid>
+      {weekdays.map((weekday) => {
+        const events = schedule[weekday] || [];
+        return (
+          <div key={weekday}>
+            <DayName>{weekday}</DayName>
+            <EventsList>
+              {events.length > 0
+                ? events.map((event) => (
+                    <Event key={`${weekday} ${event.subject}`}>
+                      <Subject>{event.subject}</Subject>
+                      <Time>
+                        <time>{event.start}</time> - <time>{event.end}</time>
+                      </Time>
+                    </Event>
+                  ))
+                : 'No events'}
+            </EventsList>
+          </div>
+        );
+      })}
+    </Grid>
+  );
 }
