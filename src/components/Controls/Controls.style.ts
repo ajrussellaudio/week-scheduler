a5d901ac2bdb3ac70f7a84cbb9fe096ff6f85b50
Alan Russell <ajrussellaudio@gmail.com>
2022-10-17 23:50:07 +0100

    Style controls

---
+++ src/components/Controls/Controls.style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 2rem 2rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,17 +1,18 @@
 import { useControls } from '../../context/Controls';
 import { AddEventModal } from '../AddEvent';
+import { Container } from './Controls.style';
 
 export type ControlsProps = Record<string, never>;
 
 export const Controls = () => {
   const { showMockEvents, toggleShowMockEvents } = useControls();
   return (
-    <div>
+    <Container>
       <AddEventModal />
       <label>
         Show mock events?
         <input type="checkbox" checked={showMockEvents} onChange={toggleShowMockEvents} />
       </label>
-    </div>
+    </Container>
   );
 };
